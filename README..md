c5bf0bb2badee0184d02b41c762b32e65c2a9831
jinjin <swj0507@gmail.com>
2024-01-09 14:48:55 +0900

    김영한님의 스프링MVC2 - 섹션2, 타임리프-스프링통합과 폼

--- README..md
+++ README..md
@@ -2,5 +2,20 @@
 - th:object는 오브젝트 정보를 넘겨주어야한다.
 - th:field = "*{itemName}" 또는 (th:field = "${item.itemName}" id,name,value를 한번에 생성해줘 간편.
 
-# 2.요구사항 추가
--
+# 2.체크박스
+<단일체크박스>
+- 체크박스 미선택 후 전송 시 필드자체가 전송안됨.
+- 해결방법 : 히든필드를 추가해 name="_open"처럼 기존체크박스앞에 '_'를 붙여 전송하면 스프링MVC는 체크를 해제했다고 인식해 open=false로 나타남.
+<다중체크박스>
+- @ModelAttribute : 반복해서 넣어주어야하는 데이터를  ModelAttribute를 사용하면 자동으로 담기게 된다.
+````
+    @ModelAttribute("regions")
+    public Map<String, String> regions(){
+        Map<String, String> regions = new LinkedHashMap<>();
+        regions.put("SEOUL", "서울");
+        regions.put("BUSAN", "부산");
+        regions.put("JEJU", "제주");
+
+        return regions;
+    }
+````
--- src/main/java/hello/itemservice/web/form/FormItemController.java
+++ src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -2,20 +2,39 @@ package hello.itemservice.web.form;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
+import hello.itemservice.domain.item.ItemType;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
+@Slf4j
 @Controller
 @RequestMapping("/form/items")
 @RequiredArgsConstructor
 public class FormItemController {
 
     private final ItemRepository itemRepository;
+    @ModelAttribute("regions")
+    public Map<String, String> regions(){
+        Map<String, String> regions = new LinkedHashMap<>();
+        regions.put("SEOUL", "서울");
+        regions.put("BUSAN", "부산");
+        regions.put("JEJU", "제주");
+
+        return regions;
+    }
+
+    @ModelAttribute("itemTypes")
+    public ItemType[] itemTypes(){
+        return  ItemType.values();
+    }
 
     @GetMapping
     public String items(Model model) {
@@ -39,6 +58,9 @@ public class FormItemController {
 
     @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+        
+        log.info("item.open={} ", item.getOpen());
+        
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
@@ -49,6 +71,8 @@ public class FormItemController {
     public String editForm(@PathVariable Long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
+
+
         return "form/editForm";
     }
 
